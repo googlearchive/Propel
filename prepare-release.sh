fef67846e5d532e9cf28829cb643e151c9e7f756
Matt Gaunt <matt@gauntface.co.uk>
2016-03-07 18:20:39 +0000

    A working viersion, errors out if any problems arise

--- prepare-release.sh
+++ prepare-release.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+# NOTES:
+#     To delete a tag use: git push origin :v1.0.0
+
 if [[ $1 != "patch" && $1 != "minor" && $1 != "major" ]] ; then
-  echo "Bad input of \"$1\"- expected an update type of \"patch\", \"minor\" or \"major\"";
+  echo "    Bad input: \"$1\". Expected an update type of \"patch\", \"minor\" or \"major\"";
   exit 1;
 fi
 
@@ -25,7 +28,7 @@ echo ""
 echo ""
 echo "Update NPM Version"
 echo ""
-npm version $1
+npm --no-git-tag-version version $1
 
 PACKAGE_VERSION=$(cat package.json \
   | grep version \
@@ -38,30 +41,35 @@ echo ""
 echo ""
 echo "Create and Copy Files for Release"
 echo ""
-mkdir -p tagged-release
+# Remove any remain artifacts from the previous build
+rm -rf ./tagged-release
+mkdir tagged-release
+
+# Copy over files that we want in the release
 cp -r ./docs ./tagged-release
 cp -r ./src ./tagged-release
 cp -r ./dist ./tagged-release
+cp LICENSE ./tagged-release
 
 cd ./tagged-release/
 
 echo ""
 echo ""
-echo "Git push to tagged-releases branch"
+echo "Git push to release branch"
 echo ""
 git init
 git remote add origin https://github.com/GoogleChrome/Propel.git
 git checkout -b release
 git add .
 git commit -m "New tagged release - $PACKAGE_VERSION"
-git tag v$PACKAGE_VERSION
+git tag -f v$PACKAGE_VERSION
 git push -f origin release v$PACKAGE_VERSION
 
 echo ""
 echo ""
-echo "Publish updat to NPM"
+echo "Publish update to NPM"
 echo ""
-# npm publish
+npm publish
 
 echo ""
 echo ""
